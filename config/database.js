b4c09828940227a1aa6d73ff4566a9b52b2c3e1d
Johann Folta <johannfolta@gmail.com>
2019-07-25 17:47:21 -0400

    add thumbs down and update DB

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 // config/database.js
 module.exports = {
-
-    'url' : 'mongodb+srv://demo:demo@cluster0-92dvh.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'demo'
+    // 'url' : 'mongodb+srv://demo:demo@cluster0-92dvh.mongodb.net/test?retryWrites=true',
+    'url' : 'mongodb+srv://johannfolta:johannfolta@21savageauthcluster-xno7i.mongodb.net/test?retryWrites=true&w=majority',
+     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+            //dbName did not matter,
+     // 'dbName': '21SavageAuthDB'
+            // it saved to "test" DB
 };
